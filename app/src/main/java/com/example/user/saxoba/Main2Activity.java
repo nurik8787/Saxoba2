21e3e477b1c9e20bd4a737a9f8e1a4eac67e1b92
nurik8787 <nuridin.anvarov@mail.ru>
2017-08-31 20:08:32 +0500

    элемент списка

--- app/src/main/java/com/example/user/saxoba/Main2Activity.java
+++ app/src/main/java/com/example/user/saxoba/Main2Activity.java
@@ -3,7 +3,11 @@ package com.example.user.saxoba;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
+
+import java.lang.reflect.Array;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,9 +17,26 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
 
-        setTitle("TabHost");
+        setTitle("Saxoba");
 
-        TabHost tabHost = (TabHost) findViewById(R.id.tabHost1); // ссылка на объект
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost1);
+        ListView novqat = (ListView) findViewById(R.id.novqat);// ссылка на объект
+        ListView yangi = (ListView) findViewById(R.id.yangi);
+        final String [] Shayx = new String[] {
+                "Ота ва Она","Зино"
+        };
+        final String [] Quran = new String[] {
+        "Ал-Фатиха","Ал-Бакара","Ал-Имран","Ан-Ниса","Ал-Маида","Ал-Анаам"
+        };
+
+        ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Shayx);
+
+        novqat.setAdapter(Adapter);
+
+        ArrayAdapter<String> Adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Quran);
+
+        yangi.setAdapter(Adapter2);
+
 
         tabHost.setup();
 
@@ -32,7 +53,7 @@ public class Main2Activity extends AppCompatActivity {
 
 
 
-        tabHost.setCurrentTab(1);
+        tabHost.setCurrentTab(0);
 
     }
 }
